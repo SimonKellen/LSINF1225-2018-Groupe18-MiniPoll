0247b525dbdf904ac4da0c2a0fe53cd5d5155b76
EmileGiot <emile.giot@student.uclouvain.be>
2018-05-04 16:30:51 +0200

    Test bouton me connecter

--- app/src/main/java/be/lsinf1225/minipoll/Activites/ConnexionActivity.java
+++ app/src/main/java/be/lsinf1225/minipoll/Activites/ConnexionActivity.java
@@ -1,16 +1,39 @@
 package be.lsinf1225.minipoll.Activites;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
 import be.lsinf1225.minipoll.R;
 
-public class ConnexionActivity extends AppCompatActivity {
+public class ConnexionActivity extends AppCompatActivity
+{
+    private Button button1;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.connexion);
+
+        button1 = (Button) findViewById(R.id.connexion1);
+        button1.setOnClickListener(new View.OnClickListener()
+        {
+
+            @Override
+            public void onClick(View v)
+            {
+                openMenuActivity();
+            }
+        });
+    }
+
+    public void openMenuActivity(){
+        Intent intent = new Intent(this, MenuPrincipalActivity.class);
+        startActivity(intent);
     }
 
 }
