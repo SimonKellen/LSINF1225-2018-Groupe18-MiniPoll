82701cc7ac8ce84cb154d538e12a3c186ac0cd72
ThomasBounoider <thomas.bounoider@student.uclouvain.be>
2018-05-06 18:49:21 +0200

    les 3 activités de créations de sondages

--- app/src/main/java/be/lsinf1225/minipoll/Activites/MenuPrincipalActivity.java
+++ app/src/main/java/be/lsinf1225/minipoll/Activites/MenuPrincipalActivity.java
@@ -13,6 +13,9 @@ public class MenuPrincipalActivity extends AppCompatActivity {
 
     private Button button1;
     private Button button2;
+    private Button button3;
+    private Button button4;
+    private Button button5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +43,39 @@ public class MenuPrincipalActivity extends AppCompatActivity {
                 openCreationActivity();
             }
         });
+
+        button3 = (Button) findViewById(R.id.menu_principal1);
+        button3.setOnClickListener(new View.OnClickListener()
+        {
+
+            @Override
+            public void onClick(View v)
+            {
+                openMenuAccordActivity();
+            }
+        });
+
+        button4 = (Button) findViewById(R.id.menu_principal2);
+        button4.setOnClickListener(new View.OnClickListener()
+        {
+
+            @Override
+            public void onClick(View v)
+            {
+                openMenuChoixActivity();
+            }
+        });
+
+        button5 = (Button) findViewById(R.id.menu_principal3);
+        button5.setOnClickListener(new View.OnClickListener()
+        {
+
+            @Override
+            public void onClick(View v)
+            {
+                openMenuQuestionnaireActivity();
+            }
+        });
     }
 
     public void openAmiActivity(){
@@ -49,6 +85,21 @@ public class MenuPrincipalActivity extends AppCompatActivity {
 
     public void openCreationActivity(){
         Intent intent = new Intent(this, CreationSondageActivity.class);
+        startActivity(intent);
+    }
+
+    public void openMenuAccordActivity(){
+        Intent intent = new Intent(this, MenuAccordActivity.class);
+        startActivity(intent);
+    }
+
+    public void openMenuChoixActivity(){
+        Intent intent = new Intent(this, MenuChoixActivity.class);
+        startActivity(intent);
+    }
+
+    public void openMenuQuestionnaireActivity(){
+        Intent intent = new Intent(this, MenuQuestionnaireActivity.class);
         startActivity(intent);
     }
 }
