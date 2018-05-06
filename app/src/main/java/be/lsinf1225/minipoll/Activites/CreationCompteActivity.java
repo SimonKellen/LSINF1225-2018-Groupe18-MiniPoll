2549c3d6ede3bbff272bd1e369eaf7b1f79a528e
ThomasBounoider <thomas.bounoider@student.uclouvain.be>
2018-05-06 19:03:15 +0200

    Activité création profil

--- app/src/main/java/be/lsinf1225/minipoll/Activites/CreationCompteActivity.java
+++ app/src/main/java/be/lsinf1225/minipoll/Activites/CreationCompteActivity.java
@@ -1,15 +1,43 @@
 package be.lsinf1225.minipoll.Activites;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import be.lsinf1225.minipoll.R;
 
 public class CreationCompteActivity extends AppCompatActivity {
 
+    private Button button1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.creation_compte);
+
+        button1 = (Button) findViewById(R.id.creation_compte1);
+        button1.setOnClickListener(new View.OnClickListener()
+        {
+
+            @Override
+            public void onClick(View v)
+            {
+                openCreationProfilActivity();
+            }
+        });
+
     }
+
+    public void openCreationProfilActivity(){
+        Intent intent = new Intent(this, CreationProfilActivity.class);
+        startActivity(intent);
+    }
+
+    public void openCreationCompteActivity(){
+        Intent intent = new Intent(this, CreationCompteActivity.class);
+        startActivity(intent);
+    }
+
 }
---
+++ app/src/main/java/be/lsinf1225/minipoll/Activites/CreationProfilActivity.java
@@ -0,0 +1,15 @@
+package be.lsinf1225.minipoll.Activites;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import be.lsinf1225.minipoll.R;
+
+public class CreationProfilActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.creation_profil);
+    }
+}
