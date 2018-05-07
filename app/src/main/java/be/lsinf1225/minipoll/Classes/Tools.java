c2b89fcf8eb33602c6b398ac6f858000c8794f82
SimonKellen <simon.kellen@student.uclouvain.be>
2018-05-07 17:07:27 +0200

    commit avant de toucher aux bdd's

--- app/src/main/java/be/lsinf1225/minipoll/Classes/Tools.java
+++ app/src/main/java/be/lsinf1225/minipoll/Classes/Tools.java
@@ -1,8 +1,19 @@
 package be.lsinf1225.minipoll.Classes;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by marti on 30-04-18.
  */
 
 public class Tools {
+}
+
+public class MySQLiteHelper extends SQLiteOpenHelper{
+    
 }
--- app/src/main/java/be/lsinf1225/minipoll/Classes/Utilisateur.java
+++ app/src/main/java/be/lsinf1225/minipoll/Classes/Utilisateur.java
@@ -1,6 +1,7 @@
 package be.lsinf1225.minipoll.Classes;
 
 import java.util.ArrayList;
+import be.lsinf1225.minipoll.Classes.Tools;
 
 /**
  * Created by Simon Kellen on 30-04-18. J'avoue c mwa ki lé fé
@@ -45,4 +46,95 @@ public class Utilisateur {
     //public void demande_ami(Utilisateur utilisateur){
         //utilisateur.ajout_Ami(this);
     //}
+
+
+    public int getId() {
+        return id;
+    }
+
+    public String getIdentifiant() {
+        return identifiant;
+    }
+
+    public ArrayList getAmis() {
+        return amis;
+    }
+
+    public String getMotDePasse() {
+        return motDePasse;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public Utilisateur getBestFriend() {
+        return bestFriend;
+    }
+
+    public ArrayList getPoll() {
+        return poll;
+    }
+
+    public ArrayList getDemandeAmis() {
+        return demandeAmis;
+    }
+
+    public void setPoll(ArrayList poll) {
+        this.poll = poll;
+    }
+
+    public void setAmis(ArrayList amis) {
+        this.amis = amis;
+    }
+
+    public void setDemandeAmis(ArrayList demandeAmis) {
+        this.demandeAmis = demandeAmis;
+    }
+
+    public void setBestFriend(Utilisateur bestFriend) {
+        this.bestFriend = bestFriend;
+    }
+
+    public void setIdentifiant(String identifiant) {
+        this.identifiant = identifiant;
+    }
+
+    public void setMotDePasse(String motDePasse) {
+        if (this.motDePasse == null) {
+            this.motDePasse = motDePasse;
+        }
+    }
+
+    public void modifierMotDePasse(String nouveauMotDePasse, String nouveauMotDePasse1, String ancienMotDePasse){
+        String ancienMotDePasse1 = "db.getancienmotdepasse";
+                if((ancienMotDePasse == ancienMotDePasse1) && (nouveauMotDePasse == nouveauMotDePasse1)){
+                    this.motDePasse = nouveauMotDePasse;
+                }
+                else{
+                    //"toast mauvais mot de passe"
+                }
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+
 }
