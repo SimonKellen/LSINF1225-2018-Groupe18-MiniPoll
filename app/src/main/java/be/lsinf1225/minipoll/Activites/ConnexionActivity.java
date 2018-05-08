7e79c22d7fd689e12e91ef028c03b99a57c2fc9b
EmileGiot <emile.giot@student.uclouvain.be>
2018-05-08 15:53:59 +0200

    ConnexionActivity + toast

--- app/src/main/java/be/lsinf1225/minipoll/Activites/ConnexionActivity.java
+++ app/src/main/java/be/lsinf1225/minipoll/Activites/ConnexionActivity.java
@@ -6,13 +6,20 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
+import java.util.ArrayList;
+
+import be.lsinf1225.minipoll.Classes.MiniPollApp;
+import be.lsinf1225.minipoll.Classes.Utilisateur;
 import be.lsinf1225.minipoll.R;
 
 public class ConnexionActivity extends AppCompatActivity
 {
     private Button button1;
     private Button button2;
+    private EditText edit1;
+    private EditText edit2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -20,18 +27,23 @@ public class ConnexionActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.connexion);
 
+        edit1 =  (EditText) findViewById(R.id.editText1);
+        edit2 =  (EditText) findViewById(R.id.editText2);
+
         button1 = (Button) findViewById(R.id.connexion1);
+        button2 = (Button) findViewById(R.id.connexion2);
+        boolean exist = Utilisateur.isUtilisateur();
+
         button1.setOnClickListener(new View.OnClickListener()
         {
 
             @Override
             public void onClick(View v)
             {
-                openMenuActivity();
+                connexionCheck();
             }
         });
 
-        button2 = (Button) findViewById(R.id.connexion2);
         button2.setOnClickListener(new View.OnClickListener()
         {
 
@@ -41,12 +53,35 @@ public class ConnexionActivity extends AppCompatActivity
                 openCreationCompteActivity();
             }
         });
+
+
+
     }
 
-    public void openMenuActivity(){
-        Intent intent = new Intent(this, MenuPrincipalActivity.class);
-        startActivity(intent);
+    public void connexionCheck()
+    {
+        String identifiant = edit1.getText().toString();
+        Utilisateur personne = Utilisateur.isUtilisateur(identifiant));
+        if(personne != null)
+        {
+            if(personne.login(edit1.getText().toString()))
+            {
+                Intent intent = new Intent(this, MenuPrincipalActivity.class);
+                startActivity(intent);
+            }
+            else
+            {
+                MiniPollApp.notifyShort(R.string.incorrect_password);
+            }
+        }
+        else
+        {
+            MiniPollApp.notifyShort(R.string.incorrect_login);
+        }
+
     }
+
+
 
     public void openCreationCompteActivity(){
         Intent intent = new Intent(this, CreationCompteActivity.class);
