ed696220fb7740dd64564f25c926d107ceb593c8
ThomasBounoider <thomas.bounoider@student.uclouvain.be>
2018-05-09 14:11:31 +0200

    Activité amis

---
+++ app/src/main/java/be/lsinf1225/minipoll/Activites/AjoutAmiActivity.java
@@ -0,0 +1,15 @@
+package be.lsinf1225.minipoll.Activites;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import be.lsinf1225.minipoll.R;
+
+public class AjoutAmiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ajout_ami);
+    }
+}
